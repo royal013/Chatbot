63c3d1d00fd07f04812033da8520cb5bce12cb04
Royal <royalbista001@gmail.com>
2024-01-02 17:42:47 +0530

    Initial commit

---
+++ response.js
@@ -0,0 +1,11 @@
+const responseObj = {
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    "foundation date":"March 2021",
+    "vision":" Foster critical thinking in students",
+    "services":"Comprehensive guidance for prestigious institutes such as IISERs, NISER, IISc Bangalore, ISI, CMI, and others",
+    "achievements":"Over 600+ selections in IISERs \nOver 100+ selections in NISER and CEBS \nAIR 1 in IAT, AIR 12 in NEST, AIR 13 in KVPY and many others "
+  };
+  
